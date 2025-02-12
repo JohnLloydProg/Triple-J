52732400019af513023db9ae34711d040fdc5b6d
Gian Mateo <gclm4002@gmail.com>
2025-02-12 10:38:37 +0800

    started the base ui design for programs page

--- Frontend_App/app/(tabs)/diet.jsx
+++ Frontend_App/app/(tabs)/diet.jsx
@@ -5,8 +5,10 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
-export default function HomeScreen() {
-
+export default function Diet() {
+  return(
+    true
+  )
 }
 
 const styles = StyleSheet.create({
