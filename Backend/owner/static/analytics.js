687a1530b7de38fc71c6bb67ca48183d89806f04
John Lloyd Unida <johnlloydunida0@gmail.com>
2025-03-15 18:19:24 +0800

    Staticfile deployments and redesigning of the owner analytics page. I have also updated the workouts list.

--- Backend/owner/static/analytics.js
+++ Backend/owner/static/analytics.js
@@ -1,154 +1,148 @@
-baseURL = "http://127.0.0.1:8000/"
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 const numberOfMembers = document.getElementById('numberOfMembers')
 const salesDaily = document.getElementById('daily')
 const salesMonthly = document.getElementById('monthly')
-const monthsSelect = document.getElementById('months')
+const monthBtns = document.getElementById('month-btns')
+const dailySalesDisplay = document.getElementById('dailySales')
+const monthlySalesDisplay = document.getElementById('monthlySales')
+
 for (let i = 0; i < 12; i++){
-    monthsSelect.insertAdjacentHTML('beforeend', `<option value="${i+1}">${months[i]}</option>`)   
-}
-
-async function refreshAccess() {
-    refreshResponse = await fetch(baseURL + 'api/account/token/refresh', {
-        method : "POST",
-        headers : {
-            "Content-Type" : "application/json",
-        },
-        body : JSON.stringify({
-            "refresh": localStorage.getItem('refreshToken')
-        })
-    }).then(response => {
-        if (!response.ok) {
-            throw new Error("Response was not ok!")
-        }
-        return response.json()
-    })
-    
-    return refreshResponse['access']
-}
-
-async function request(url) {
-    try {
-        responseData = await fetch(url, {
-            method : "GET",
-            headers : {
-                "Content-Type" : "application/json",
-                "Authorization" : `Bearer ${localStorage.getItem('accessToken')}`,
-            },
-            credentials : 'same-origin'
-        }).then(response => {
-            if (!response.ok) {
-                throw new Error('Request was not ok!')
-            }
-            return response.json()
-        })
-        return responseData
-    }catch(err) {
-        try {
-            accessToken = await refreshAccess()
-            localStorage.setItem('accessToken', accessToken)
-            return request(url)
-        }catch(err1) {
-            throw new Error('Refresh token is expired or invalid!')
-        }
-    }
+  monthBtns.insertAdjacentHTML('beforeend', `<a onclick="selectMonth(${i+1})">${months[i]}</a>`)   
 }
 
 async function initGraph(month) {
-    salesDaily.innerHTML = ""
-    salesMonthly.innerHTML = ""
+  salesDaily.innerHTML = ""
+  salesMonthly.innerHTML = ""
+  let monthlySales = 0
+  let dailySales = 0
 
-    peakHoursData = await request(baseURL + `api/analytics/peak/${month}/hours`)
-    console.log(peakHoursData)
-    new Chart("peakHours", {
-        type: "line",
-        data: {
-            labels:peakHoursData['x'],
-            datasets:[{
-                fill:false,
-                lineTension:0,
-                backgroundColor: "rgba(0, 0, 255, 1.0)",
-                borderColor: "rgba(0, 0, 255, 0.1)" ,
-                data:peakHoursData['y']
-            }]
+  peakHoursData = await myRequest.get(`api/analytics/peak/${month}/hours`)
+  console.log(peakHoursData)
+  new Chart("peakHours", {
+      type: "line",
+      data: {
+          labels:peakHoursData['x'],
+          datasets:[{
+              fill:false,
+              lineTension:0,
+              backgroundColor: "rgba(234, 68, 68, 1.0)",
+              borderColor: "rgba(234, 68, 68, 0.1)" ,
+              data:peakHoursData['y']
+          }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          }
         },
-        options: {}
-      });
+      }
+    });
 
-      peakDaysData = await request(baseURL + `api/analytics/peak/${month}/days`)
-      console.log(peakDaysData)
-      new Chart("peakDays", {
-          type: "line",
-          data: {
-              labels:peakDaysData['x'],
-              datasets:[{
-                  fill:false,
-                  lineTension:0,
-                  backgroundColor: "rgba(0, 0, 255, 1.0)",
-                  borderColor: "rgba(0, 0, 255, 0.1)" ,
-                  data:peakDaysData['y']
-              }]
+  peakDaysData = await myRequest.get(`api/analytics/peak/${month}/days`)
+  console.log(peakDaysData)
+  new Chart("peakDays", {
+      type: "line",
+      data: {
+          labels:peakDaysData['x'],
+          datasets:[{
+              fill:false,
+              lineTension:0,
+              backgroundColor: "rgba(234, 68, 68, 1.0)",
+              borderColor: "rgba(234, 68, 68, 0.1)" ,
+              data:peakDaysData['y']
+          }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          }
+        },
+      }
+    });
+
+    membersData = await myRequest.get('api/analytics/members/report')
+    console.log(membersData)
+    numberOfMembers.textContent = membersData['number']
+    new Chart("demographics", {
+        type: "pie",
+        data: {
+          labels: ['Male', 'Female'],
+          datasets: [{
+            backgroundColor: ["#EA4444", "#FF2626", "#EA9744", "#756A6A", "#B50000", "#360000"],
+            data: [membersData['demographics']['M'], membersData['demographics']['F']]
+          }]
+        },
+        options: {
+          legend: {
+            position: 'left',
+            align: 'start', 
           },
-          options: {}
-        });
-
-        membersData = await request(baseURL + 'api/analytics/members/report')
-        console.log(membersData)
-        numberOfMembers.textContent = `Number of Members: ${membersData['number']}`
-        new Chart("demographics", {
-            type: "pie",
-            data: {
-              labels: ['Male', 'Female'],
-              datasets: [{
-                backgroundColor: ["#8080FF", "#FC0FC0"],
-                data: [membersData['demographics']['M'], membersData['demographics']['F']]
-              }]
-            },
-            options: {}
-          });
-        new Chart("memberships", {
-            type: "pie",
-            data: {
-              labels: ['Monthly', 'Daily'],
-              datasets: [{
-                backgroundColor: ["#8080FF", "#FC0FC0"],
-                data: [membersData['memberships']['Monthly'], membersData['memberships']['Daily']]
-              }]
-            },
-            options: {}
-          });
-        
-        workoutsData = await request(baseURL + 'api/analytics/program/types')
-        console.log(workoutsData)
-        new Chart("workouts", {
-            type: "pie",
-            data: {
-              labels: workoutsData['x'],
-              datasets: [{
-                backgroundColor: ["#FFF1C9", "#F7B7A3", "#EA5F89", "#9B3192", "#57167E", "#2B0B3F"],
-                data: workoutsData['y']
-              }]
-            },
-            options: {}
-          });
-        
-        salesReport = await request(baseURL + `api/analytics/sales/${month}`)
-        console.log(salesReport)
-        for (const [key, value] of Object.entries(salesReport['monthly'])) {
-            salesMonthly.insertAdjacentHTML('beforeend', `<tr><td style="widght: 70%;">${key}</td> <td style="width: 30%;">${value}</td></tr>`)
+          elements: {
+            arc: {
+              
+            }
+          }
         }
-        for (const [key, value] of Object.entries(salesReport['daily'])) {
-            salesDaily.insertAdjacentHTML('beforeend', `<tr><td style="widght: 70%;">${key}</td> <td style="width: 30%;">${value}</td></tr>`)
+      });
+    new Chart("memberships", {
+        type: "pie",
+        data: {
+          labels: ['Monthly', 'Daily'],
+          datasets: [{
+            backgroundColor: ["#EA4444", "#FF2626", "#EA9744", "#756A6A", "#B50000", "#360000"],
+            data: [membersData['memberships']['Monthly'], membersData['memberships']['Daily']]
+          }]
+        },
+        options: {
+          legend: {
+            position: 'left',
+            align: 'start', 
+          }
         }
-}
+      });
+    
+    workoutsData = await myRequest.get('api/analytics/program/types')
+    console.log(workoutsData)
+    new Chart("workouts", {
+        type: "pie",
+        data: {
+          labels: workoutsData['x'],
+          datasets: [{
+            backgroundColor: ["#EA4444", "#FF2626", "#EA9744", "#756A6A", "#B50000", "#360000"],
+            data: workoutsData['y']
+          }]
+        },
+        options: {
+          legend: {
+            position: 'left',
+            align: 'start', 
+          }
+        }
+      });
+    
+    salesReport = await myRequest.get(`api/analytics/sales/${month}`)
+    console.log(salesReport)
+    for (const [key, value] of Object.entries(salesReport['monthly'])) {
+      monthlySales += value
+      salesMonthly.insertAdjacentHTML('beforeend', `<tr><td style="widght: 70%; padding-left: 10px">${key}</td> <td style="width: 30%; text-align: right; padding-right: 10px;">${value}</td></tr>`)
+    }
+    monthlySalesDisplay.textContent = monthlySales
 
-async function selectMonth() {
-    console.log(monthsSelect.value)
-    initGraph(parseInt(monthsSelect.value))
+    for (const [key, value] of Object.entries(salesReport['daily'])) {
+      dailySales += value
+      salesDaily.insertAdjacentHTML('beforeend', `<tr><td style="widght: 70%; padding-left: 10px">${key}</td> <td style="width: 30%; text-align: right; padding-right: 10px;">${value}</td></tr>`)
+    }
+    dailySalesDisplay.textContent = dailySales
+  }
+  
+async function selectMonth(month) {
+  console.log(month)
+  initGraph(parseInt(month))
 }
 
 let month = new Date().getMonth()+1
 
 initGraph(month)
-monthsSelect.value = month
 
--- Backend/staticfiles/analytics.js
+++ Backend/staticfiles/analytics.js
@@ -2,138 +2,147 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 const numberOfMembers = document.getElementById('numberOfMembers')
 const salesDaily = document.getElementById('daily')
 const salesMonthly = document.getElementById('monthly')
-const monthsSelect = document.getElementById('months')
+const monthBtns = document.getElementById('month-btns')
+const dailySalesDisplay = document.getElementById('dailySales')
+const monthlySalesDisplay = document.getElementById('monthlySales')
+
 for (let i = 0; i < 12; i++){
-    monthsSelect.insertAdjacentHTML('beforeend', `<option value="${i+1}">${months[i]}</option>`)   
+  monthBtns.insertAdjacentHTML('beforeend', `<a onclick="selectMonth(${i+1})">${months[i]}</a>`)   
 }
 
 async function initGraph(month) {
-    salesDaily.innerHTML = ""
-    salesMonthly.innerHTML = ""
+  salesDaily.innerHTML = ""
+  salesMonthly.innerHTML = ""
+  let monthlySales = 0
+  let dailySales = 0
 
-    peakHoursData = await myRequest.get(`api/analytics/peak/${month}/hours`)
-    console.log(peakHoursData)
-    new Chart("peakHours", {
-        type: "line",
+  peakHoursData = await myRequest.get(`api/analytics/peak/${month}/hours`)
+  console.log(peakHoursData)
+  new Chart("peakHours", {
+      type: "line",
+      data: {
+          labels:peakHoursData['x'],
+          datasets:[{
+              fill:false,
+              lineTension:0,
+              backgroundColor: "rgba(234, 68, 68, 1.0)",
+              borderColor: "rgba(234, 68, 68, 0.1)" ,
+              data:peakHoursData['y']
+          }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          }
+        },
+      }
+    });
+
+  peakDaysData = await myRequest.get(`api/analytics/peak/${month}/days`)
+  console.log(peakDaysData)
+  new Chart("peakDays", {
+      type: "line",
+      data: {
+          labels:peakDaysData['x'],
+          datasets:[{
+              fill:false,
+              lineTension:0,
+              backgroundColor: "rgba(234, 68, 68, 1.0)",
+              borderColor: "rgba(234, 68, 68, 0.1)" ,
+              data:peakDaysData['y']
+          }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          }
+        },
+      }
+    });
+
+    membersData = await myRequest.get('api/analytics/members/report')
+    console.log(membersData)
+    numberOfMembers.textContent = membersData['number']
+    new Chart("demographics", {
+        type: "pie",
         data: {
-            labels:peakHoursData['x'],
-            datasets:[{
-                fill:false,
-                lineTension:0,
-                backgroundColor: "rgba(0, 0, 255, 1.0)",
-                borderColor: "rgba(0, 0, 255, 0.1)" ,
-                data:peakHoursData['y']
-            }]
+          labels: ['Male', 'Female'],
+          datasets: [{
+            backgroundColor: ["#EA4444", "#FF2626", "#EA9744", "#756A6A", "#B50000", "#360000"],
+            data: [membersData['demographics']['M'], membersData['demographics']['F']]
+          }]
         },
         options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            }
+          legend: {
+            position: 'left',
+            align: 'start', 
           },
+          elements: {
+            arc: {
+              
+            }
+          }
         }
       });
-
-      peakDaysData = await myRequest.get(`api/analytics/peak/${month}/days`)
-      console.log(peakDaysData)
-      new Chart("peakDays", {
-          type: "line",
-          data: {
-              labels:peakDaysData['x'],
-              datasets:[{
-                  fill:false,
-                  lineTension:0,
-                  backgroundColor: "rgba(0, 0, 255, 1.0)",
-                  borderColor: "rgba(0, 0, 255, 0.1)" ,
-                  data:peakDaysData['y']
-              }]
-          },
-          options: {
-            scales: {
-              y: {
-                beginAtZero: true,
-              }
-            },
+    new Chart("memberships", {
+        type: "pie",
+        data: {
+          labels: ['Monthly', 'Daily'],
+          datasets: [{
+            backgroundColor: ["#EA4444", "#FF2626", "#EA9744", "#756A6A", "#B50000", "#360000"],
+            data: [membersData['memberships']['Monthly'], membersData['memberships']['Daily']]
+          }]
+        },
+        options: {
+          legend: {
+            position: 'left',
+            align: 'start', 
           }
-        });
-
-        membersData = await myRequest.get('api/analytics/members/report')
-        console.log(membersData)
-        numberOfMembers.textContent = `Number of Members: ${membersData['number']}`
-        new Chart("demographics", {
-            type: "pie",
-            data: {
-              labels: ['Male', 'Female'],
-              datasets: [{
-                backgroundColor: ["#8080FF", "#FC0FC0"],
-                data: [membersData['demographics']['M'], membersData['demographics']['F']]
-              }]
-            },
-            options: {
-              legend: {
-                position: 'left',
-                align: 'start', 
-              },
-              elements: {
-                arc: {
-                  borderColor: '#EA4444'
-                }
-              }
-            }
-          });
-        new Chart("memberships", {
-            type: "pie",
-            data: {
-              labels: ['Monthly', 'Daily'],
-              datasets: [{
-                backgroundColor: ["#8080FF", "#FC0FC0"],
-                data: [membersData['memberships']['Monthly'], membersData['memberships']['Daily']]
-              }]
-            },
-            options: {
-              legend: {
-                position: 'left',
-                align: 'start', 
-              }
-            }
-          });
-        
-        workoutsData = await myRequest.get('api/analytics/program/types')
-        console.log(workoutsData)
-        new Chart("workouts", {
-            type: "pie",
-            data: {
-              labels: workoutsData['x'],
-              datasets: [{
-                backgroundColor: ["#FFF1C9", "#F7B7A3", "#EA5F89", "#9B3192", "#57167E", "#2B0B3F"],
-                data: workoutsData['y']
-              }]
-            },
-            options: {
-              legend: {
-                position: 'left',
-                align: 'start', 
-              }
-            }
-          });
-        
-        salesReport = await myRequest.get(`api/analytics/sales/${month}`)
-        console.log(salesReport)
-        for (const [key, value] of Object.entries(salesReport['monthly'])) {
-            salesMonthly.insertAdjacentHTML('beforeend', `<tr><td style="widght: 70%;">${key}</td> <td style="width: 30%;">${value}</td></tr>`)
         }
-        for (const [key, value] of Object.entries(salesReport['daily'])) {
-            salesDaily.insertAdjacentHTML('beforeend', `<tr><td style="widght: 70%;">${key}</td> <td style="width: 30%;">${value}</td></tr>`)
+      });
+    
+    workoutsData = await myRequest.get('api/analytics/program/types')
+    console.log(workoutsData)
+    new Chart("workouts", {
+        type: "pie",
+        data: {
+          labels: workoutsData['x'],
+          datasets: [{
+            backgroundColor: ["#EA4444", "#FF2626", "#EA9744", "#756A6A", "#B50000", "#360000"],
+            data: workoutsData['y']
+          }]
+        },
+        options: {
+          legend: {
+            position: 'left',
+            align: 'start', 
+          }
         }
-}
+      });
+    
+    salesReport = await myRequest.get(`api/analytics/sales/${month}`)
+    console.log(salesReport)
+    for (const [key, value] of Object.entries(salesReport['monthly'])) {
+      monthlySales += value
+      salesMonthly.insertAdjacentHTML('beforeend', `<tr><td style="widght: 70%; padding-left: 10px">${key}</td> <td style="width: 30%; text-align: right; padding-right: 10px;">${value}</td></tr>`)
+    }
+    monthlySalesDisplay.textContent = monthlySales
 
-async function selectMonth() {
-    console.log(monthsSelect.value)
-    initGraph(parseInt(monthsSelect.value))
+    for (const [key, value] of Object.entries(salesReport['daily'])) {
+      dailySales += value
+      salesDaily.insertAdjacentHTML('beforeend', `<tr><td style="widght: 70%; padding-left: 10px">${key}</td> <td style="width: 30%; text-align: right; padding-right: 10px;">${value}</td></tr>`)
+    }
+    dailySalesDisplay.textContent = dailySales
+  }
+  
+async function selectMonth(month) {
+  console.log(month)
+  initGraph(parseInt(month))
 }
 
 let month = new Date().getMonth()+1
 
 initGraph(month)
-monthsSelect.value = month
 
