5e054ffe0901eaec699ca52dfa1259ed5d8b39cf
JohnLloydProg <johnlloydunida0@gmail.com>
2025-05-27 03:32:00 +0800

    Fixed the registration routing

--- Backend/request_test.js
+++ Backend/request_test.js
@@ -1,13 +1,16 @@
 let baseURL = "https://triple-j.onrender.com/";
 
-fetch(baseURL + "api/account/token", {
+fetch(baseURL + "api/account/registration/ee7bdef5-2982-46a9-99ef-12746d077cd9", {
     method : "POST",
     headers : {
-        "Content-Type" : "application/json"
+        "Content-Type" : "application/json",
+        "Authorization": `Bearer `
     },
     body : JSON.stringify({
-        'username':'admin',
-        'password':'admin12345'
+        'email':'gmateo4002@gmail.com',
+        'username':'its_mateo',
+        'password':'Mateo12345',
+        'membership':'Monthly'
     }),
     credentials: 'same-origin',
 }).then((response) => {
@@ -17,30 +20,6 @@ fetch(baseURL + "api/account/token", {
     return response.json();
 }).then((data) => {
     console.log(data);
-
-    fetch(baseURL + "api/account/email-validation", {
-        method : "POST",
-        headers : { 
-            "Authorization": `Bearer ${data['access']}`,
-            "Content-Type" : "application/json"
-        },
-        body : JSON.stringify({
-            'email': 'gmateo4002@gmail.com'
-        }),
-        credentials: 'same-origin'
-    }).then((response) => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-    
-        if (response.headers.get('Content-Type') == "application/json") {
-            return response.json();
-        }else {
-            return response.blob();
-        }
-    }).then((data) => {
-        console.log(data);    
-    }).catch((error) => console.error("Fetch error", error));
 }).catch(
     (error) => console.error("Fetch error", error)
 );
