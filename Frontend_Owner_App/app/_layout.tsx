0743eedadeaa330c8d93bd3abd3887deb9c64983
JohnLloydProg <johnlloydunida0@gmail.com>
2025-05-27 01:04:15 +0800

    QR Scanning is working just need to add the details to the view. Changed the backend API for the logging service to return the needed details.

--- Frontend_Owner_App/app/_layout.tsx
+++ Frontend_Owner_App/app/_layout.tsx
@@ -4,6 +4,6 @@ import './globals.css'
 export default function RootLayout() {
   return <Stack>
     <Stack.Screen name='index' options={{ headerShown: false }}/>
-    <Stack.Screen name='(tabs)' options={{ headerShown: false }}/>
+    <Stack.Screen name='profile' options={{ headerShown: false }}/>
   </Stack>;
 }
