d2a6b559213db35892ace8fc6ce7733ea6091247
JohnLloydProg <johnlloydunida0@gmail.com>
2025-06-04 23:36:29 +0800

    Remove the unique property for the receipt_no attribute on the Sale table

--- Backend/sales/models.py
+++ Backend/sales/models.py
@@ -7,7 +7,7 @@ class Sale(models.Model):
     date = models.DateField(auto_now=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.CharField(max_length=255, null=True, blank=True)
-    receipt_no = models.CharField(max_length=50, unique=True, null=True, blank=True)
+    receipt_no = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return f"{self.date} - {self.amount} - {self.description}"
